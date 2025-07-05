af1ba238c451bd3fb5c1b1758108be7bf064fce3
Tanny <1441049296@qq.com>
2025-07-05 12:22:47 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'EtButton',
+})
+</script>
+
+<template>
+  <button style="color: red;">Hellow World</button>
+</template>
---
+++ packages/core/components.ts
@@ -0,0 +1,4 @@
+import { EtButton } from "@toy-element/components";
+import type { Plugin } from "vue";
+
+export default [EtButton] as Plugin[];
